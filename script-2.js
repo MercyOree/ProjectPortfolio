600dae55d9e6ec01c7e3ade14b69b24aaf954464
MercyOree <115586010+MercyOree@users.noreply.github.com>
2025-02-20 23:51:09 -0600

    Add files via upload

---
+++ script-2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('contact-btn').addEventListener('click', function () {
+        alert('Feel free to reach out at mercyorekoku@gmail.com!');
+    });
+});
